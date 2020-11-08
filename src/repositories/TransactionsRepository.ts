5c215a20d0891d9a1525bd8b329f6b15282f68c2
Andrey Casetta <andrey_amc@hotmail.com>
2020-11-07 23:53:14 -0300

    Finalizado desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,45 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    value = Math.abs(value);
+
+    this.transactions.push(transaction);
+
+    if (type === 'income') this.balance.income += value;
+    else this.balance.outcome += value;
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return transaction;
   }
 }
 
